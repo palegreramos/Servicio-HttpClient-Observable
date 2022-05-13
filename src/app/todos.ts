df4b7e5ccc47dd422bb16dd5d2eb4f35adb0c275
palegreramos <palegre@iespuntadelverde.es>
2022-05-13 11:48:06 +0200

    1 file modified

--- src/app/todos.ts
+++ src/app/todos.ts
@@ -4,3 +4,9 @@ export interface Todos {
   title: string;
   completed: boolean;
 }
+
+export class Todos {
+
+  constructor(public userId:number,public id:number, public title:string, public completed: boolean){}
+
+}
