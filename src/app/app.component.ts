c897a307545fb0d61039f76129fe10ce9b576220
palegreramos <palegre@iespuntadelverde.es>
2022-05-19 13:29:12 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,29 +10,24 @@ import { Observable} from 'rxjs';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-//public items=new Array<Todos>();
+
  public items$: Observable<Todos[]>;
 
 
   constructor (private servicioService: ServicioService) {}
 
   ngOnInit() {
-      // this.servicioService.getData()
-      //    .subscribe((item:Todos[]) => 
-      //      this.items=item.map((i)=>{
-      //        let user=new Todos(i.userId,i.id,i.title,i.completed);
-      //        return user;
-      //      }));
+    
      this.items$ = this.servicioService.getData();
  
   }
 
   cambiaEstado(item:Todos) {
-  // item.cambia();
+
  
   item.completed=!item.completed;
   console.log(item)
-  // console.log(item.mays)
+
  }
 
   
