461350eb9370a63abba5c9f19deb26738f1d12e7
palegreramos <palegre@iespuntadelverde.es>
2022-05-21 16:48:47 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
 import { ServicioService } from './servicio.service';
-import { ResumenComponent } from './resumen/resumen.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, ResumenComponent ],
+  declarations: [ AppComponent ],
   providers: [ ServicioService ], 
   bootstrap:    [ AppComponent ]
 })
