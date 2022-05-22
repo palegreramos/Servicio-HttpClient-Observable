308f2eca5e8b0479066acc921904ff1c95d8fa9b
palegreramos <palegre@iespuntadelverde.es>
2022-05-22 08:16:00 +0200

    2 files modified

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  public userForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() {
   }
-
+  createForm() {
+    this.userForm=this.fb.group({
+      nombre: ['', [Validators.required]],
+      pass1: ['', [Validators.required,Validators.pattern('[0-9a-zA-Z]{2,4}')]],
+      pass2: ['',[Validators.required,this.matchValidator]]
+    });
+  }
+  createUser() {
+    if (this.userForm.valid) console.log("Usuario creado",this.userForm.value)
+  }
+  matchValidator(control: AbstractControl) {
+    if (control.get('pass1').value==control.get('pass2').value)
+    control.get('pass2').setErrors({ NoPassswordMatch: true });
+  }
+  
 }
