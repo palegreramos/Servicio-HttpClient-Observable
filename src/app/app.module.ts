29596d633849393965946cafdab86a1cf85e66c2
palegreramos <palegre@iespuntadelverde.es>
2022-05-22 07:09:09 +0200

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,18 @@ import { AppComponent } from './app.component';
 import { ListadoComponent } from './listado/listado.component';
 import { TablaComponent } from './tabla/tabla.component';
 import { UsersComponent } from './users/users.component';
+import { FormularioComponent } from './formulario/formulario.component';
 import { ServicioService } from './servicio.service';
 
 const routes: Routes = [
   { path: "tabla", component: TablaComponent },
-  { path: "users", component: UsersComponent }];
+  { path: "users", component: UsersComponent },
+  { path: "form", component: FormularioComponent }
+];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, ListadoComponent, TablaComponent, UsersComponent ],
+  declarations: [ AppComponent, ListadoComponent, TablaComponent, UsersComponent, FormularioComponent ],
   providers: [ ServicioService ], 
   bootstrap:    [ AppComponent ]
 })
